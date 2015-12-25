4edb90697998cee3b492af23aed9fe5435fcd44f
熊谷 典利 <kumagai@gstaff.co.jp>
2015-12-25 21:05:02 +0900

    modified contact_controller

--- web/controllers/contact_controller.ex
+++ web/controllers/contact_controller.ex
@@ -4,6 +4,6 @@ defmodule DevPhoenix.ContactController do
     use DevPhoenix.Web, :controller
 
     def index(conn, _params) do
-        json conn, %{contacts: %{id: 1, name: "noritoshi kumagai", tel: "+01-2345-4567", country: "JP"}}
+        json conn, %{contacts: %{id: 1, name: "noritoshi kumagai", tel: "+01-2345-5678", country: "JP"}}
     end
 end
